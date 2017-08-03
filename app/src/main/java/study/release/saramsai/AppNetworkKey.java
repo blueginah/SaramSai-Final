2f91c5fec26b8c74a645b58bc1a39779198419a4
Tronze <0183535663@naver.com>
2017-08-03 18:58:43 +0900

    Finished Video List Part, but has some tasks for server connection part

---
+++ app/src/main/java/study/release/saramsai/AppNetworkKey.java
@@ -0,0 +1,15 @@
+package study.release.saramsai;
+
+/**
+ * Created by Tronze on 2017-08-03.
+ */
+
+public class AppNetworkKey {
+
+    private static final String APP_KEY = "SaramsaiUser1707";
+
+    public static String getAppKey() {
+        return APP_KEY;
+    }
+
+}
